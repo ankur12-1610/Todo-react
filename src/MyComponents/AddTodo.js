36aff521495579a149133ce30455d43067759010
Nathália Costa <62223406+Nthliacc@users.noreply.github.com>
2021-10-16 16:39:22 -0300

    spelling correction

--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -32,7 +32,7 @@ export const AddTodo = ({addTodo}) => {
             <input  value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="title" aria-describedby="emailHelp"/>
         </div>
         <div className="mb-3">
-            <label htmlFor="desc" className="form-label">Todo Decsription</label>
+            <label htmlFor="desc" className="form-label">Todo Description</label>
             <input  value={desc} type="text"onChange={(e)=>{setDesc(e.target.value)}} className="form-control" id="desc"/>
         </div>
         <button type="submit" className="btn btn-info" style={buttoncss}>Add Todo</button>
