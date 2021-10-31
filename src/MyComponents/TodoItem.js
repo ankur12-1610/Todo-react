1e39893499827da1d7f8498b3b68c6d5e345e9b9
Santi <santiagojimenez.quintero@gmail.com>
2021-10-31 19:49:34 +0100

    Resolve merge

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -3,17 +3,19 @@ import PropTypes from "prop-types";
 
 export const TodoItem = ({ todo, onDelete }) => {
   return (
-    <div>
-      <h4>{todo.title}</h4>
-      <p>{todo.desc}</p>
-      <button
-        className="btn btm-sm btn-danger"
-        onClick={() => {
-          onDelete(todo);
-        }}
-      >
-        Delete
-      </button>
+    <div className="card bg-light mb-3 ">
+      <div className="card-body">
+        <h4 className="card-title">{todo.title}</h4>
+        <p className="card-text">{todo.desc}</p>
+        <button
+          className="btn btm-sm btn-danger"
+          onClick={() => {
+            onDelete(todo);
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
