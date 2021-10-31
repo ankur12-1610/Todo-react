d713303b0d49afb71d1ee9ea41970636fb57180c
Santi <santiagojimenez.quintero@gmail.com>
2021-10-31 19:56:21 +0100

    Resolve merge conflicts

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -4,44 +4,47 @@ import { FaCheese } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 
 export const Footer = () => {
-  let footerStyle = {
-    positon: "relative",
-    margin: "23vh 0px 0px 0px",
-  };
   let font = {
     color: "#53b3cb",
   };
   return (
     <footer
       className="bg-dark sticky-bottom text-light py-3 "
-      style={footerStyle}
+      style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        textAlign: "center",
+      }}
     >
-      <div>
-        {/* <p className="text-center">
-            Copyright &copy; Ankur
-            </p> */}
-      </div>
-
-      <div className="text-center">
-        <p>
-          Made with <FaCheese /> and <FaHeart />
-        </p>
-      </div>
-      <div className="text-center">
-        <p>
-          For code, visit github repo{" "}
-          <a href="https://github.com/ankur12-1610/Todo-react/">
-            <FaGithub color="#53b3cb" />
-          </a>
-        </p>
-      </div>
-      <div className="text-center">
-        <p>
-          Copyright &copy;{" "}
-          <a href="https://itsankur.tech/" style={font}>
-            Ankur
-          </a>
-        </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
+      >
+        <div className="text-center">
+          <p>
+            For code, visit github repo{" "}
+            <a href="https://github.com/ankur12-1610/Todo-react/">
+              <FaGithub color="#53b3cb" />
+            </a>
+          </p>
+        </div>
+        <div className="text-center">
+          <p>
+            Made with <FaCheese /> and <FaHeart />
+          </p>
+        </div>
+        <div className="text-center">
+          <p>
+            Copyright &copy;{" "}
+            <a href="https://itsankur.tech/" style={font}>
+              Ankur
+            </a>
+          </p>
+        </div>
       </div>
     </footer>
   );
